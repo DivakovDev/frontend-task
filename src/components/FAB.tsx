24069524c9dce5f416c0188a0752b9142891aee3
Lyusi Divakov <divakovwork@gmail.com>
2025-03-18 19:50:54 +0200

    Make FAB button to lead to banner creation page

--- src/components/FAB.tsx
+++ src/components/FAB.tsx
@@ -1,15 +1,34 @@
 import { Button } from '@mui/joy'
 import Add from '@mui/icons-material/Add'
+import { useNavigate } from 'react-router-dom'
 
 export default function FAB() {
+    const navigate = useNavigate()
     return (
-        <Button style={{
-            position: 'fixed',
-            right: '72px',
-            bottom: '32px',
-            height: '72px',
-            borderRadius: '50%',
-        }} size="lg">
+        <Button
+            onClick={() => navigate('/banners/create')}
+            sx={{
+                position: 'fixed',
+                bottom: {
+                  xs: 15, 
+                  sm: 30, 
+                },
+                right: {
+                  xs: 15,
+                  sm: 30,
+                },
+                borderRadius: '50%',
+                width: {
+                    xs: 56,
+                    md: 72,
+                  },
+                height:{
+                    xs: 56,
+                    md: 72,
+                  },
+              }}
+            size="lg"
+        >
             <Add />
         </Button>
     )
